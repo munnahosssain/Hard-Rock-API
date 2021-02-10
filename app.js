3134e4869ba14cceb13ec61bb50ebfcb20351f72
Munna Hossain <mayinuddin.munna@gmail.com>
2021-02-10 21:42:24 +0600

    everything is up to date without (lyric lineup)

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+// searchSongs = async () => {
+//     const searchText = document.getElementById('search-field').value;
+//     const url = `https://api.lyrics.ovh/suggest/${searchText}`
+//     //loading data
+//     const res = await fetch(url);
+//     const data = await res.json();
+//     displaySongs(data.data);
+// }
 searchSongs = () => {
     const searchText = document.getElementById('search-field').value;
     const url = `https://api.lyrics.ovh/suggest/${searchText}`
@@ -5,16 +13,20 @@ searchSongs = () => {
     fetch(url)
         .then(res => res.json())
         .then(data => displaySongs(data.data))
+        .catch(error => displayError("Something went wrong!"));
+
 }
 const displaySongs = songs => {
     const songContainer = document.getElementById('song-container');
+    songContainer.innerHTML = '';
+
     songs.forEach(song => {
         const songDiv = document.createElement('div');
         songDiv.className = 'single-result row align-items-center my-3 p-3';
         songDiv.innerHTML = `
             <div class="col-md-9">
                 <h3 class="lyric-name">${song.title}</h3>
-                <p class="author load">Album by-s<span>${song.artist.name}</span></p>
+                <p class="author load">Album by- <span>${song.artist.name}</span></p>
                 <audio controls>
                     <source src="${song.preview}" type="audio/mpeg">
                 </audio>
@@ -26,6 +38,29 @@ const displaySongs = songs => {
         songContainer.appendChild(songDiv);
     })
 }
-const getLyric = (artist, title) => {
-    console.log(artist, title);
+const getLyric = async (artist, title) => {
+    const url = `https://api.lyrics.ovh/v1/${artist}/${title}`
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        displayLyric(data.lyrics);
+    }
+    catch (error) {
+        displayError('Sorry! i failed to load lyric');
+    }
+
+}
+// const getLyric = (artist, title) => {
+//     const url = `https://api.lyrics.ovh/v1/${artist}/${title}`
+//     fetch(url)
+//         .then(res => res.json())
+//         .then(data => displayLyric(data.lyrics));
+// }
+const displayLyric = lyrics => {
+    const lyricsDiv = document.getElementById('song-lyrics');
+    lyricsDiv.innerHTML = lyrics;
+}
+const displayError = error => {
+    const errorTag = document.getElementById('error-message');
+    errorTag.innerText = error;
 }
